8e21593c91e37557f5ab9608f1ad0b10cbf38241
PonnalaAbhinav <abhinavponnala51@gmail.com>
2023-07-14 00:39:36 +0530

    5th commit

--- api/routes/conversations.js
+++ api/routes/conversations.js
@@ -1,17 +1,17 @@
-const router=require("express").Router();
-const Conversation=require("../models/Conversation");
+const router = require("express").Router();
+const Conversation = require("../models/Conversation");
 
 //new conversation
 
-router.post("/",async (req,res)=>{
-    const newConversation=new Conversation({
-        members:[req.body.senderId,req.body.receiverId]
+router.post("/", async (req, res) => {
+    const newConversation = new Conversation({
+        members: [req.body.senderId, req.body.receiverId]
     });
 
-    try{
-        const savedConversation=await newConversation.save();
+    try {
+        const savedConversation = await newConversation.save();
         res.status(200).json(savedConversation);
-    }catch(err){
+    } catch (err) {
         res.status(500).json(err);
     }
 })
@@ -19,15 +19,28 @@ router.post("/",async (req,res)=>{
 
 //get conversation
 
-router.get("/:userId",async (req,res)=>{
-    try{
-        const conversation=await Conversation.find({
-            members:{ $in: [req.params.userId]}
+router.get("/:userId", async (req, res) => {
+    try {
+        const conversation = await Conversation.find({
+            members: { $in: [req.params.userId] }
         });
         res.status(200).json(conversation);
-    }catch(err){
+    } catch (err) {
         res.status(500).json(err);
     }
 })
 
-module.exports=router;
+//get conversation includes two userId
+router.get("/find/:firstUserId/:secondUserId", async (req, res) => {
+    try {
+        const conversation = await Conversation.findOne({
+            members: { $all:  [req.params.firstUserId, req.params.secondUserId]  }
+        })
+        res.status(200).json(conversation);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;
+
