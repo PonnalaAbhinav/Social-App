ed256ff063b16ecf9bb26af0b624a0090d88d134
PonnalaAbhinav <abhinavponnala51@gmail.com>
2023-07-13 19:31:01 +0530

    3rd commit

--- api/index.js
+++ api/index.js
@@ -7,6 +7,8 @@ const morgan=require("morgan");
 const userRoute=require("./routes/users")
 const authenticationRoute=require("./routes/authentication")
 const postRoute=require("./routes/posts");
+const conversationRoute=require("./routes/conversations");
+const messageRoute=require("./routes/messages");
 const multer=require('multer');
 const path=require("path")
 
@@ -41,6 +43,8 @@ app.post("/api/upload",upload.single("file"),(req,res)=>{
 app.use("/api/users",userRoute);
 app.use("/api/authentication",authenticationRoute);
 app.use("/api/posts",postRoute);
+app.use("/api/conversations",conversationRoute);
+app.use("/api/messages",messageRoute);
 
 
 app.listen(3000,()=>{
